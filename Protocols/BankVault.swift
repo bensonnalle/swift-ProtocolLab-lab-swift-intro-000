d576bbcc11459a76953c7cc9ecbf6b32f3514026
Ben <bennalle1@gmail.com>
2017-03-08 10:35:09 -0500

    passes

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,7 +20,30 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
 
-
+extension BankVault: ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        var access: Bool = false
+        if password.count <= 10 && password.count > 0 {
+            let copyPassword = password
+            if copyPassword[0] % 2 == 0 {
+                for (index, item) in copyPassword.enumerated() {
+                    if index % 2 == 0 && item % 2 == 0 {
+                        access = true
+                    }
+                }
+            }
+        } else {
+            access = false
+        }
+        return access
+    }
+    
+}
 
 
